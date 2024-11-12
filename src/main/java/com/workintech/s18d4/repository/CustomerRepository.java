c05d43912136d12b13ad2a6db625c285a9ed7672
ezgikardes <ezgikardes@gmail.com>
2024-11-12 22:40:39 +0100

    repos added

--- src/main/java/com/workintech/s18d4/repository/CustomerRepository.java
+++ src/main/java/com/workintech/s18d4/repository/CustomerRepository.java
@@ -4,4 +4,5 @@ import com.workintech.s18d4.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
 }
